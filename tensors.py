42e36498f33590604274d3b013841eb809e96587
adkrueger <adkrueger@wpi.edu>
2021-02-28 23:53:16 -0500

    completed tensors portion of tutorial

--- tensors.py
+++ tensors.py
@@ -23,6 +23,57 @@ rand_tensor = torch.rand(shape)
 ones_tensor = torch.ones(shape)
 zeros_tensor = torch.zeros(shape)
 
-print(f"Random Tensor: \n {rand_tensor} \n")
-print(f"Ones Tensor: \n {ones_tensor} \n")
-print(f"Zeros Tensor: \n {zeros_tensor}")
+print(f'Random Tensor: \n {rand_tensor} \n')
+print(f'Ones Tensor: \n {ones_tensor} \n')
+print(f'Zeros Tensor: \n {zeros_tensor}\n')
+
+# tensors have attributes like shape, datatype, and the device they're stored on
+tensor = torch.rand(3, 4)
+print(f'Shape of tensor: {tensor.shape}')
+print(f'Datatype of tensor: {tensor.dtype}')
+print(f'Device tensor is stored on: {tensor.device}\n')
+
+# there's also many tensor operations: https://pytorch.org/docs/stable/torch.html
+# for example, moving operations to the GPU:
+if torch.cuda.is_available():
+    tensor = tensor.to('cuda')
+
+# similar operations to numpy
+tensor = torch.ones(4, 4)
+tensor[:, 1] = 0
+print(tensor)
+
+# torch.cat will concatenate a sequence of tensors along a given dimension
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
+
+# a few ways to compute the element-wise product:
+print(f'tensor.mul(tensor):\n {tensor.mul(tensor)}\n')
+print(f'tensor * tensor:\n {tensor * tensor}\n')
+
+# alternatively, matrix multiplication
+print(f'tensor.matmul(tensor.T):\n {tensor.matmul(tensor.T)} \n')
+print(f'tensor @ tensor.T: \n {tensor @ tensor.T}\n')
+
+# there's also in-place operations that have a _ suffix
+# (note that these save memory, but can be problematic with derivatives because we lose history)
+print(tensor, '\n')
+tensor.add_(5)
+print(tensor)
+
+# tensors on CPU and numpy arrays can share underlying memory locations so that changing one will change the other
+t = torch.ones(5)
+print(f'\nt: {t}')
+n = t.numpy()
+print(f'n: {n}')
+# now that it's bridged, lets see the changes:
+t.add_(1)
+print(f't: {t}')
+print(f'n: {n}')
+
+# numpy array to tensor
+n = np.ones(5)
+t = torch.from_numpy(n)
+np.add(n, 1, out=n)
+print(f't: {t}')
+print(f'n: {n}')
